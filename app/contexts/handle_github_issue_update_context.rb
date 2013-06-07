de0e6426ed08036c55dc645683ddbfab61e3a612
Alvaro Crespo <alvaro.andres.crespo@gmail.com>
2013-06-07 14:08:53 -0300

    Solved bug involving modification of estimated_hours. Closes#31.

--- app/contexts/handle_github_issue_update_context.rb
+++ app/contexts/handle_github_issue_update_context.rb
@@ -1,16 +1,25 @@
 class HandleGithubIssueUpdateContext
 
-  def initialize(github, issue)
+  def initialize(github, issue, params)
     @github = github
     @issue = issue
+    @params = params
+    @params.delete(:github)
+    @params.delete(:project)
+    @params.delete(:worked_hours)
+
+    #Rails generates a hash with an string keys, and the method resourse_params in 
+    #the issue controller has a white list with symbols.
+    #This line converts the string keys to symbols.
+    @params = @params.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
   end
 
   def handle_issue
     raise ArgumentError, "Project cannot be nil" unless @issue
     GithubProvisioner::Issue.new(@github, @issue).sync_status!
-    @issue.save!
-  rescue
-    false
+    @issue.update_attributes(@params)
+    rescue
+      false
   end
 
 
